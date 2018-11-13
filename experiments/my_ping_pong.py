d70c83a421bd1769b5e02e6d8d656bf6eb4da159
vindard <17693119+vindard@users.noreply.github.com>
2018-11-13 12:22:03 -0400

    fix socketserver import bug

--- experiments/my_ping_pong.py
+++ experiments/my_ping_pong.py
@@ -1,17 +1,17 @@
-import SocketServer
+import socketserver
 
 host = "0.0.0.0"
 port = 10000
 address = (host, port)
 
-class  TCPHandler(SocketServer.BaseRequestHandler):
+class  TCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         message = self.request.recv(10)
         print(f"got a message: {message}")
 
 def serve():
-    server = SocketServer.TCPServer(address, TCPHandler)
+    server = socketserver.TCPServer(address, TCPHandler)
     server.serve_forever()
 
 if __name__ == "__main__":
