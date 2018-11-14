d5d801628d21d0495331fd6475fbeb9c6802a2d3
vindard <17693119+vindard@users.noreply.github.com>
2018-11-13 22:43:37 -0400

    insert networking funtionality into mybanknetcoin

--- mybanknetcoin.py
+++ mybanknetcoin.py
@@ -1,4 +1,4 @@
-import uuid
+import uuid, socketserver, socket, sys
 from copy import deepcopy
 from ecdsa import SigningKey, SECP256k1
 from utils import serialize
@@ -104,3 +104,40 @@ class Bank:
         unspents = self.fetch_utxo(public_key)
         # Sum the amounts
         return sum([tx_out.amount for tx_out in unspents])
+
+
+host = "0.0.0.0"
+port = 10000
+address = (host, port)
+
+class MyTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
+class  TCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        message = self.request.recv(10).strip()
+        print(f"got a message: {message}")
+
+        if message == b'ping':
+            self.request.sendall(b"pong\n")
+
+def serve():
+    server = MyTCPServer(address, TCPHandler)
+    server.serve_forever()
+
+def ping():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(address)
+    sock.sendall(b"ping")
+    data = sock.recv(10)
+    print(f"Received {data.decode()}")
+
+if __name__ == "__main__":
+    command = sys.argv[1]
+    if command == "serve":
+        serve()
+    elif command == "ping":
+        ping()
+    else:
+        print(f"Invalid command.")
