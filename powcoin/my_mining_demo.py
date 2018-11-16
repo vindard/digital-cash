8c2d14b68904ba88c237bf69a981c22107b65c3a
vindard <17693119+vindard@users.noreply.github.com>
2018-11-16 13:19:58 -0400

    add visuals to create mining demo

--- powcoin/my_mining_demo.py
+++ powcoin/my_mining_demo.py
@@ -1,4 +1,4 @@
-import hashlib
+import time, hashlib
 
 
 
@@ -8,18 +8,25 @@ def get_proof(header, nonce):
     return int(proof_hex, 16)
 
 
-def mine(header, targer):
+def mine(header, target):
     nonce = 0
     while get_proof(header, nonce) >= target:
         nonce += 1 # new guess
     return nonce
 
 
+def mining_demo(header):
+    for difficulty_bits in range(1, 30):
+        target = 2 ** (256 - difficulty_bits)        
+
+        start_time = time.time()
+        nonce = mine(header,target)
+        proof = get_proof(header, nonce)
+        elapsed_time = time.time() - start_time
+
+        print(f"bits: {difficulty_bits} target: {target} elapsed time: {elapsed_time} nonce: {nonce} proof: {proof}\n")
+
 
 if __name__ == "__main__":
     header = "hello"
-    # number of leading bits we require
-    difficulty_bits = 8
-    target = 2 ** (256 - difficulty_bits)
-    nonce = mine(header, target)
-    print(nonce)
+    mining_demo(header)
